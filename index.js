c5772a9685ed953ea6fdfdadc5c39ddcbcbe55a1
Danilo <danilorocha722@gmail.com>
2023-08-22 21:02:48 -0300

    Ordenação de listagem, criação de paginas para cada pergunta

--- index.js
+++ index.js
@@ -6,23 +6,34 @@ const Pergunta = require('./database/pergunta');
 
 connection
     .authenticate()
-    .then(()=>{
+    .then(() => {
         console.log("Conexão feita com o banco de dados")
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err)
     })
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get("/", (req,res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+    Pergunta.findAll({
+        raw: true, order: [
+            ['id', 'desc']
+        ]
+    })
+        .then(perguntas => {
+            res.render('index', {
+                perguntas: perguntas
+            });
+        });
+    var perguntas = [];
+
 })
 
-app.get("/perguntar", (req,res) => {
+app.get("/perguntar", (req, res) => {
     res.render('perguntar');
 })
 
@@ -32,9 +43,25 @@ app.post('/perguntar', (req, res) => {
     Pergunta.create({
         titulo: titulo,
         descricao: descricao
-    }).then(()=>{
+    }).then(() => {
         res.redirect("/")
     })
 })
 
-app.listen(3000, () => {console.log("Server Running")});
+app.get("/pergunta/:id", (req, res) => {
+    var id = req.params.id;
+    var pergunta;
+    Pergunta.findOne({
+        where: { id: id }
+    }).then(pergunta => {
+        if (pergunta != undefined) {
+            res.render('pergunta', {
+                pergunta: pergunta
+            });
+        } else {
+            res.redirect("/")
+        }
+    })
+})
+
+app.listen(3000, () => { console.log("Server Running") });
